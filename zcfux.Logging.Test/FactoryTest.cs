898b59eefa7e80b38cf2f5c7cd55da331759b6ec
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2021-12-26 16:52:26 +0100

    Logging: unit tests

---
+++ zcfux.Logging.Test/FactoryTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace zcfux.Logging.Test;
+
+public sealed class FactoryTest
+{
+    [Test]
+    public void CreateLogger()
+    {
+        var logger = Factory.ByName("void");
+
+        Assert.IsInstanceOf<ILogger>(logger);
+    }
+
+    [Test]
+    public void CreateUnknownLogger()
+    {
+        var loggerName = TestContext.CurrentContext.Random.GetString();
+
+        Assert.Throws<FactoryException>(() => Factory.ByName(loggerName));
+    }
+}

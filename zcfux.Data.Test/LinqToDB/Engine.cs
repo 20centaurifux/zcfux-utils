116afce451b32c33f169d4527970e2f36f44cd86
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2022-02-06 15:03:55 +0100

    Data: LinqToDB unit tests

---
+++ zcfux.Data.Test/LinqToDB/Engine.cs
@@ -0,0 +1,47 @@
+﻿/***************************************************************************
+    begin........: December 2021
+    copyright....: Sebastian Fedrau
+    email........: sebastian.fedrau@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 3 of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ ***************************************************************************/
+using LinqToDB.Configuration;
+using LinqToDB.Mapping;
+
+namespace zcfux.Data.Test.LinqToDB;
+
+public class Engine : zcfux.Data.LinqToDB.Engine
+{
+    public Engine(LinqToDbConnectionOptions opts)
+        : base(opts)
+    {
+    }
+
+    public override void Setup()
+    {
+        var mappingSchema = new MappingSchema();
+        var builder = mappingSchema.GetFluentMappingBuilder();
+
+        builder.Entity<Model>()
+            .HasTableName("Model")
+            .HasIdentity(m => m.ID)
+            .HasPrimaryKey(m => m.ID)
+            .Property(m => m.Value);
+
+        MappingSchema.Default = builder.MappingSchema;
+    }
+}
---
+++ zcfux.Data.Test/LinqToDB/TestDb.cs
@@ -0,0 +1,54 @@
+﻿/***************************************************************************
+    begin........: December 2021
+    copyright....: Sebastian Fedrau
+    email........: sebastian.fedrau@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 3 of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ ***************************************************************************/
+using LinqToDB;
+using zcfux.Data.LinqToDB;
+
+namespace zcfux.Data.Test.LinqToDB;
+
+internal sealed class TestDb : ITestDb
+{
+    void ITestDb.DeleteAll(object handle)
+        => DeleteAll((handle as Handle)!);
+
+    static void DeleteAll(Handle handle)
+        => handle.Db()
+            .GetTable<Model>()
+            .Delete();
+
+    public Model New(object handle, string value)
+        => New((handle as Handle)!, value);
+
+    static Model New(Handle handle, string value)
+    {
+        var model = new Model() { Value = value };
+
+        model.ID = Convert.ToInt32(handle.Db().InsertWithIdentity(model));
+
+        return model;
+    }
+
+    public IEnumerable<Model> All(object handle)
+        => All((handle as Handle)!);
+
+    static IEnumerable<Model> All(Handle handle)
+        => handle.Db().GetTable<Model>();
+}
